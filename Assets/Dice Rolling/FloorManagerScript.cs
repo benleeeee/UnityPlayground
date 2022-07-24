dca52665cb5c7d645af8d5e7ec745e5a89d004ce
misterpicklez <leeben118@gmail.com>
2022-07-24 23:29:11 +0100

    Added infrastructure for managing spawned tiles & added tile spawning animation

--- Assets/Dice Rolling/FloorManagerScript.cs
+++ Assets/Dice Rolling/FloorManagerScript.cs
@@ -11,38 +11,53 @@ using UnityEngine;
 /// </summary>
 public class FloorManagerScript : MonoBehaviour
 {
+    const float TileSpawnHeight = 0f;
     public GameObject _Sensor;
     public GameObject _BlackTile;
     public GameObject _WhiteTile;
     public Transform _SensorParent;
     public Transform _FloorParent;
 
+    private Dictionary<Vector2, Transform> _CoordToTileMap = new Dictionary<Vector2, Transform>();
+    private Dictionary<Vector2, Transform> _CoordToSensorMap = new Dictionary<Vector2, Transform>();
     private void Start()
     {
-        SpawnSensor();
+        //Add initial tiles
+        _CoordToTileMap.Add(new Vector2(0, 0), _FloorParent.transform.Find("BlackTile"));
+        SpawnInitialSensors();
     }
-    private void SpawnSensor()
+    private void SpawnInitialSensors()
     {
-        GameObject sensor = GameObject.Instantiate(_Sensor, new Vector3(5.514f, 0, 0), Quaternion.identity, _SensorParent);
+        SpawnSensor(new Vector2(0, 1), true);
+        SpawnSensor(new Vector2(1, 0), true);
+        SpawnSensor(new Vector2(0, -1), true);
+        SpawnSensor(new Vector2(-1, 0), true);
+        SpawnSensor(new Vector2(1, 1), false);        
+        SpawnSensor(new Vector2(-1, 1), false);
+        SpawnSensor(new Vector2(1, -1), false);
+        SpawnSensor(new Vector2(-1, -1), false);
+    }
+    private void SpawnSensor(Vector2 worldPos, bool BlackOrWhite)
+    {
+        //TODO: Use object pooling here instead of instantiating new sensors every time
+        GameObject sensor = GameObject.Instantiate(_Sensor, new Vector3(worldPos.x, 0, worldPos.y), Quaternion.identity, _SensorParent);
         sensor.GetComponent<TileSpawnSensor>().SetFloorManager(this);
-        sensor.GetComponent<TileSpawnSensor>().SetColour(false);
-        sensor = GameObject.Instantiate(_Sensor, new Vector3(5.514f + (1 * 1), 0, 0), Quaternion.identity, _SensorParent);
-        sensor.GetComponent<TileSpawnSensor>().SetFloorManager(this);
-        sensor.GetComponent<TileSpawnSensor>().SetColour(true);
-        sensor = GameObject.Instantiate(_Sensor, new Vector3(5.514f + (2 * 1), 0, 0), Quaternion.identity, _SensorParent);
-        sensor.GetComponent<TileSpawnSensor>().SetFloorManager(this);
-        sensor.GetComponent<TileSpawnSensor>().SetColour(false);
-        sensor = GameObject.Instantiate(_Sensor, new Vector3(5.514f + (3 * 1), 0, 0), Quaternion.identity, _SensorParent);
-        sensor.GetComponent<TileSpawnSensor>().SetFloorManager(this);
-        sensor.GetComponent<TileSpawnSensor>().SetColour(true);
+        sensor.GetComponent<TileSpawnSensor>().SetColour(BlackOrWhite);
+        _CoordToSensorMap.Add(worldPos, sensor.transform);
     }
     public void SpawnTile(Vector3 worldPos, bool blackOrWhite)
     {
         //TODO: Use object pooling here instead of instantiating new tiles every time
         if (blackOrWhite)
-            GameObject.Instantiate(_WhiteTile, worldPos, Quaternion.identity, _FloorParent);
+        {
+            GameObject t = GameObject.Instantiate(_WhiteTile, new Vector3(worldPos.x, TileSpawnHeight, worldPos.z), Quaternion.identity, _FloorParent);
+            t.transform.GetChild(0).GetComponent<Animation>().Play("FlyUp");
+        }
         else
-            GameObject.Instantiate(_BlackTile, worldPos, Quaternion.identity, _FloorParent);
+        {
+            GameObject t = GameObject.Instantiate(_BlackTile, new Vector3(worldPos.x, TileSpawnHeight, worldPos.z), Quaternion.identity, _FloorParent);
+            t.transform.GetChild(0).GetComponent<Animation>().Play("FlyUp");
+        }
     }
     private void RemoveTile()
     {
